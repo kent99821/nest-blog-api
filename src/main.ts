4f181070a461a98f062a73b7df12504e094d60b3
kent99821 <61462124+kent99821@users.noreply.github.com>
2021-08-16 18:01:23 +0800

    完成基本的架构

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest博客API')
+    .setDescription('我的第一个nest项目')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,23 +1,55 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
 
+
+class CreatePostDto {
+    @ApiProperty({description:'帖子标题'})
+    title:string
+    @ApiProperty({description:'帖子内容'})
+    content:string
+}
+@ApiTags('帖子')
 @Controller('posts')
 export class PostsController {
     @Get()
-    index(){
-        return [1,2,3]
+    @ApiOperation({ summary: "显示帖子列表" })
+    index() {
+        return [1, 2, 3]
     }
 
     @Post()
-    create(){
+    @ApiOperation({ summary: "创建帖子" })
+    create(@Body() body:CreatePostDto) { //参数装饰器
+        body
         return {
-            success:true
+            success: true
         }
     }
     @Get(':id')
-    detail(){
+    @ApiOperation({summary:"帖子详情"})
+    detail(@Param('id') id:string) {
         return {
-            id:1,
-            title:'aaaqa'
+            id: id,
+            title: 'aaaqa'
+        }
+    }
+    @Put(':id')
+    @ApiOperation({summary:"编辑帖子"})
+    update(@Param('id') id:string, @Body() body:CreatePostDto){
+
+        return {
+            success:true,
+            id:id,
+            title:body.title,
+            content:body.content
+        }
+    }
+    @Delete(':id')
+    @ApiOperation({summary:"删除帖子"})
+    remove(@Param('id') id:string){
+        return {
+            success:true,
+            id:id
         }
     }
 
